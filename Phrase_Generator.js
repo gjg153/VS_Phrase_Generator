79909dcc8e9b6bb00a5a4522c27a776c2924498f
gjg153 <greg.gustitis@gmail.com>
2021-02-10 20:39:02 -0500

    Phrase generator working correctly

--- Phrase_Generator.js
+++ Phrase_Generator.js
@@ -1,4 +1,13 @@
-const num = Math.floor(Math.random()*11);
-console.log(num);
+function ranNum(num) {
+    return Math.floor(Math.random()*num)
+};
 
+let names = ['Joe','Bob','John','Steve'];
+let feelings = ['happy','sad','proud','disappointed'];
+let events = ['studies','sleeps','eats','relaxes'];
 
+const person = names[ranNum(4)];
+const emotion = feelings[ranNum(4)];
+const action = events[ranNum(4)];
+
+console.log(`${person} feels ${emotion} when he ${action}`);
